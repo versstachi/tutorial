8a7c65aec0b8466f342ac8f9439c473e1ffa1d32
versstachi <versstachi@gmail.com>
2021-05-09 15:51:15 +0300

    Исправление  2021_05_2_5

--- 2021_05_2__5/common.js
+++ 2021_05_2__5/common.js
@@ -1,23 +1,21 @@
 let numberArr = [];
 for ( var i = 0; i < 10; i++ ) {
 	numberArr.push(Math.floor( Math.random() * 10 ));
-}   
+}
 console.log(numberArr);
 let numbCheck;  
-let countNumb = {x:'0',}; 
+let countNumb = 0; 
 let numbCheckVall;  
 for (i = 0; i < numberArr.length; i++) { 
 	numbCheckVall = 0; 
-   	numbCheck = numberArr[i];      
-    console.log("Проверяем число в масиве: " + numbCheck);  
+	numbCheck = numberArr[i];      
+	console.log("Проверяем число в масиве: " + numbCheck);  
 	for (x = 0; x < numberArr.length; x++) {  
-	   	if(numbCheck == numberArr[x]) {
-	   		numbCheckVall += 1; 
+		if(numbCheck == numberArr[x]) {
+			numbCheckVall += 1; 
 		}
-    }   
-    countNumb = {
-    	numbCheck: numbCheckVall,
-    }
-	console.log("Повторяется раз: " + countNumb.numbCheck);
+	}
+	countNumb = numbCheckVall;
+	console.log("Повторяется раз: " + countNumb);
 	console.log("");
-}  
+}
