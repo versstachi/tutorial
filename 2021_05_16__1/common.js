3b995e64b5bd778fe94a4ab45d8a5b704075e90d
versstachi <versstachi@gmail.com>
2021-05-16 18:00:23 +0300

    Task 2021_05_16__1

--- 2021_05_16__1/common.js
+++ 2021_05_16__1/common.js
@@ -1,13 +1,14 @@
 let numberArr = [1,2,3,4,4,3,2,1];
 console.log(numberArr); 
-let recerse = [1,2,3,4,4,3,2,1,]; 
+let recerse = [1,2,3,4,4,3,2,1]; 
 console.log(recerse);  
 
-// let numbCheck = numberArr;
-for(i=0;i<numberArr.length;++i){    
+let validefalse = false;
+for(i=0;i<numberArr.length;++i){ 
+	validefalse = numberArr[i]; 
 }
-if(recerse[(numberArr.length-1)-i] === numberArr[i] && recerse.length==numberArr.length){   
-	console.log("Есть сходство");
+if(recerse[(numberArr.length-1)-i] === numberArr[i] && recerse.length==numberArr.length && validefalse){   
+	console.log("Массив симетричен");
 } 
 else{
 	console.log("Массив не симетричен"); 
